3f389b0ff0211b7a01c09d5d70b814454354b6b5
cristinaleticia <leticia.c.silva@ufv.br>
2025-05-17 19:32:46 -0300

    Estrutura de dados

---
+++ modulos/EstruturaDeDados/Fila.c
@@ -0,0 +1,151 @@
+#include "Fila.h" 
+
+// Funcao para criar e inicializar uma nova fila
+Fila* criaFila(){
+
+    // Aloca memoria para a estrutura da fila
+    Fila* fila = (Fila*) malloc(sizeof(Fila));
+    fila->Frente = (Apontador) malloc(sizeof(CelulaProcesso)); // Celula cabeca
+
+    
+    fila->Tras = fila->Frente; // Inicializa o ponteiro Tras apontando para a mesma celula que Frente, as duas apontam para a celula cabeca
+    
+    fila->Frente->Prox = NULL;  // Define que a celula inicial n possui proximo elemento, pq a fila ta vazia
+
+    fila->Tamanho = 0; 
+    return fila; // Retorna o ponteiro para a nova fila criada
+} 
+
+// Funcao para verificar se uma fila esta vazia
+int filaEhVazia(Fila* Fila){   
+    return (Fila->Tamanho == 0); // Retorna 1 se o tamanho da fila for 0 (fila esta vazia), caso contrario retorna 0
+}
+
+// Funcao para verificar se todas as filas de um conjunto estao vazias
+int filasVazias(Fila** filas, int numFilas){
+    for (int i = 0; i < numFilas; i++){
+        if (!filaEhVazia(filas[i])){
+            return 0; // Se encontrar uma fila nao vazia, retorna 0
+        }
+    }
+    return 1;  // Se todas as filas estiverem vazias, retorna 1
+}
+
+
+// Funcao para adicionar um elemento na fila
+void enfileirar(int pid, int tempoExecutado, Fila *Fila){
+    Fila->Tras->Prox = (Apontador) malloc(sizeof(CelulaProcesso)); // Liga a nova celula na fila
+    Fila->Tras = Fila->Tras->Prox; // Atualiza o ponteiro Tras para a nova celula, ela passa a ser a ultima 
+    Fila->Tras->Prox = NULL; // A ultima celula n possui proximo elemento
+    Fila->Tras->Processo = criaCelulaProcesso(pid, tempoExecutado);
+    
+    Fila->Tamanho++;
+}
+
+// Funcao para remover o primeiro elemento da fila e retornar seus dados
+Processo* desenfileirar(Fila* fila){ //OLHAR MELHOR DEPOIS
+    if (filaEhVazia(fila)) {
+        return NULL;
+    }
+
+    Processo* ProcessoRemovido = (Processo*) malloc(sizeof(Processo));
+
+    CelulaProcesso* celulaRemovida = fila->Frente;
+
+    fila->Frente = fila->Frente->Prox;  // Atualiza o ponteiro Frente para o proximo elemento
+
+    // Copia os dados do elemento da frente da fila
+
+    ProcessoRemovido->pid = fila->Frente->Processo.pid;
+    ProcessoRemovido->tempoExecutado = fila->Frente->Processo.tempoExecutado;
+    
+    free(celulaRemovida);
+
+    // Se a fila ficou vazia, atualiza o ponteiro Tras para NULL
+    if (filaEhVazia){
+        fila->Tras = NULL; 
+    }
+
+    fila->Tamanho--;
+    return ProcessoRemovido;
+}
+
+
+// Funcao para remover o primeiro elemento da fila e retornar apenas o PID.
+int desenfileirarPID(Fila* fila){
+    if (filaEhVazia(fila)){
+        return -1;
+    }
+    int processoRemovido = fila->Frente->Processo.pid; // Armazena o PID do elemento da frente da fila
+
+    CelulaProcesso* celulaRemovida = fila->Frente; 
+    fila->Frente = fila->Frente->Prox; // Atualiza o ponteiro Frente para o proximo elemento
+    free(celulaRemovida);
+
+
+    // Se a fila ficou vazia, atualiza o ponteiro Tras para NULL
+    if (filaEhVazia){
+        fila->Tras = NULL;
+    }
+
+    fila->Tamanho--;
+    return processoRemovido;
+}
+
+// Funcao para remover o primeiro elemento da primeira fila nao vazia de um conjunto de filas
+int desenfileirarFilas(Fila** filas, int numFilas){
+    int pidProcessoRemovido = -1, i = 0;
+
+    while (pidProcessoRemovido == -1 && i < numFilas){ // Itera pelas filas ate encontrar uma n vazia
+        pidProcessoRemovido = desenfileirarPID(filas[i]); // Tenta remover o elemento da fila atual
+
+        if (pidProcessoRemovido >= 0){ // Se encontrou um elemento, retorna o PID
+            return pidProcessoRemovido;
+        } else{
+            i++;
+        }
+    }
+
+    return pidProcessoRemovido;
+}
+
+// Funcao para criar uma celula com os dados de PID e tempo executado
+Processo criaCelulaProcesso(int PID, int tempoExecutado){
+    Processo celula;
+    celula.pid = PID;
+    celula.tempoExecutado = tempoExecutado;
+
+    return celula;
+}
+
+// Funcao para imprimir os elementos de uma fila
+void imprimeFila(Fila *fila){
+    if (filaEhVazia(fila)){
+        printf("\n  Fila esta Vazia :( \n");
+
+    } else {
+        CelulaProcesso *celula = fila->Frente; // Ponteiro para iterar pelos elementos da fila
+
+        while (celula != NULL) {
+            if (celula->Processo.tempoExecutado == -1){ // Se o tempo executado for -1, imprime apenas o PID
+                printf("\n   Pid: %d", celula->Processo.pid);
+            } else{
+                printf("\n   Pid: %d, Tempo bloqueado: %d", celula->Processo.pid, celula->Processo.tempoExecutado);
+            }
+            celula = celula->Prox;
+        }
+        putchar('\n');
+    }
+}
+
+void imprimeFilas(Fila** filas, int numFilas){
+// Funcao para imprimir os elementos de um conjunto de filas
+    for (int i = 0; i < numFilas; i++){
+        Fila* fila = filas[i];
+
+        printf("\nFila numero: %d:", i);
+
+
+        imprimeFila(filas[i]); // Chama a funcao de imprimir fila para cada fila do conjunto
+    }
+}
---
+++ modulos/EstruturaDeDados/Fila.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//"Apontador" eh um ponteiro para uma estrutura do tipo "CelulaProcesso"
+typedef struct CelulaProcesso* Apontador;
+
+typedef struct Processo {
+    int pid; // "pid" (identificador de processo)
+    int tempoExecutado; // "tempoExecutado" (tempo que o processo ja executou).
+} Processo;
+
+typedef struct CelulaProcesso {
+    Processo Processo;
+    Apontador Prox;
+} CelulaProcesso;
+
+typedef struct Fila {
+    Apontador Frente, Tras;
+    int Tamanho;
+} Fila;
+
+Fila* criaFila();
+
+int filaEhVazia(Fila* Fila);
+int filasVazias(Fila** filas, int numFilas); // Verifica se todas as filas em um conjunto de filas estao vazias
+void enfileirar(int pid, int tempoExecutado, Fila *Fila);
+Processo* desenfileirar(Fila* fila); // Remove e retorna o primeiro elemento da fila
+int desenfileirarPID(Fila* fila); // Remove o primeiro elemento da fila de filas e retorna o Processo
+int desenfileirarFilas(Fila** filas, int numFilas); // Esvazia todas as filas da fila
+Processo criaCelulaProcesso(int PID, int tempoExecutado);
+void imprimeFila(Fila *fila);
+void imprimeFilas(Fila** filas, int numFilas);
